37b310561f610eb46c75e5785991d09718b5fb58
Yue Liu <yue.liu@mail.com>
2017-07-09 22:39:31 -0700

    Setup dependency and main window

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate gtk;
+extern crate gdk;
+extern crate pango;
+
+use gtk::prelude::*;
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+
+    window.set_title("Type Holder");
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(640, 480);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    let header_bar = gtk::HeaderBar::new();
+    header_bar.set_show_close_button(true);
+    let aliases_combo_box = gtk::ComboBoxText::new();
+    let add_button = gtk::Button::new();
+    let dup_button = gtk::Button::new();
+    let del_button = gtk::Button::new();
+
+    header_bar.pack_start(&aliases_combo_box);
+    header_bar.pack_start(&add_button);
+    header_bar.pack_start(&dup_button);
+    header_bar.pack_start(&del_button);
+    window.set_titlebar(&header_bar);
+
+    window.show_all();
+    gtk::main();
+}
